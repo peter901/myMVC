209822935a81bbb0d81d28f5d38a684e13a84d97
Peter Ahumuza <hmz2peter@gmail.com>
2022-09-09 08:59:26 +0300

    initial commit

---
+++ app/controllers/Posts.php
@@ -0,0 +1,16 @@
+<?php
+
+class Posts{
+
+    public function __construct(){
+
+    }
+
+    public function index(){
+        
+    }
+
+    public function about($param = null){
+        echo "about method in posts loaded with param {$param}";
+    }
+}
