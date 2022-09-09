33a674beaad81b596b78c834ec783e2a163a1553
Peter Ahumuza <hmz2peter@gmail.com>
2022-09-09 09:26:33 +0300

    Changes to be committed: 	modified:   app/libraries/Core.php

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -42,12 +42,11 @@ class Core {
 
     public function getUrl(){
 
-        if(isset($_GET['url'])){
-            $url =rtrim($_GET['url'],'/');
-            $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode('/',$url);
+        if(isset($_GET['url']))
+        {
+            $url =explode('/',filter_var(rtrim($_GET['url'],'/'), FILTER_SANITIZE_URL));
             return $url;
         }
-        
+
     }
 }
